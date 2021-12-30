4e5b48ed8e084b3ad597c42d4a1c6e01a43b80f1
Nolan Mitchell <yetigus69@gmail.com>
2021-12-30 00:24:57 -0400

    Blob's Payload Sender 1.5.0

--- Payload Sender 3/Properties/AssemblyInfo.cs
+++ Payload Sender 3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blob's Payload Sender")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Blob's Payload Sender 1.5.0")]
+[assembly: AssemblyDescription("An Application To Send .bin Payloads From A Windows System To An Exploited Playstation 4 Console")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blob's Payload Sender")]
+[assembly: AssemblyCompany("TheMagicalBlob")]
+[assembly: AssemblyProduct("Blob's Payload Sender 1.5.0")]
 [assembly: AssemblyCopyright("Copyright ©  2077")]
 [assembly: AssemblyTrademark("TheMagicalBlob")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.6.0")]
-[assembly: AssemblyFileVersion("1.4.6.0")]
+[assembly: AssemblyVersion("1.5.0.2")]
+[assembly: AssemblyFileVersion("1.5.0.2")]
