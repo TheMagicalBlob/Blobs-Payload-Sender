43f1b08ea72c33f69ec480a0eb069aeb9a61655d
Nolan Mitchell <yetigus69@gmail.com>
2021-12-30 00:45:38 -0400

    Blob's Payload Sender 1.5.0 r4

--- Payload Sender 3/Properties/AssemblyInfo.cs
+++ Payload Sender 3/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("An Application To Send .bin Payloads From A Windows System To An Exploited Playstation 4 Console")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TheMagicalBlob")]
-[assembly: AssemblyProduct("Blob's Payload Sender 1.5.0")]
+[assembly: AssemblyProduct("Blob's Payload Sender 1.5.0 R4")]
 [assembly: AssemblyCopyright("Copyright ©  2077")]
 [assembly: AssemblyTrademark("TheMagicalBlob")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.2")]
-[assembly: AssemblyFileVersion("1.5.0.2")]
+[assembly: AssemblyVersion("1.5.0.5")]
+[assembly: AssemblyFileVersion("1.5.0.5")]
